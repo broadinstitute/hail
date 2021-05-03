e0c3c1b514244da312099c46b90fd2e5938d717d
Carolin Diaz <63973811+CDiaz96@users.noreply.github.com>
2021-05-03 14:19:01 -0400

    [WIP]experimenting with plotly

--- batch/batch/front_end/front_end.py
+++ batch/batch/front_end/front_end.py
@@ -1409,6 +1409,12 @@ async def ui_get_job(request, userdata, batch_id):
         resources['actual_cpu'] = resources['cores_mcpu'] / 1000
         del resources['cores_mcpu']
 
+    import plotly.express as px
+    df = px.data.gapminder()
+    fig = px.line(df, x="year", y="lifeExp", color="continent", line_group="country", hover_name="country",
+        line_shape="spline", render_mode="svg")
+    plot_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
     page_context = {
         'batch_id': batch_id,
         'job_id': job_id,
@@ -1418,7 +1424,10 @@ async def ui_get_job(request, userdata, batch_id):
         'step_statuses': step_statuses,
         'job_specification': job_specification,
         'job_status_str': json.dumps(job, indent=2),
+        'plot_json':plot_json,
     }
+
+
     return await render_template('batch', request, userdata, 'job.html', page_context)
 
 
